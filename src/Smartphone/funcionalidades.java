52ccceb035bb1ff5e568e77bd631cb1e08ff9eb0
Jeff-desv <brunojeff0012@gmail.com>
2024-05-20 19:53:20 -0300

    iniciando as funcionalidades

--- src/Smartphone/funcionalidades.java
+++ src/Smartphone/funcionalidades.java
@@ -1,5 +1,21 @@
 package Smartphone;
 
-public class funcionalidades {
+public abstract class funcionalidades {
+	
+	//Reprodutor Musical
+	public abstract void tocar();
+	
+	public abstract void pausar();
+	
+	public void selecionarMusica() {
+		System.out.println("Nothing Else Matters");
+	}
+	
+	//Aparelho Telefonico
+	public void ligar() {
+		System.out.println("Ligando para: 8661-8953");
+	}
+	
+	
 
 }
